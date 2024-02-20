fc2cb41835edb362fc3cc24039c97b5f57ffa185
ezeokolichukwuemerie <ezeokolichukwuemerie@yahoo.com>
2024-02-20 21:22:59 +0100

    ussd commit

---
+++ ussd.js
@@ -0,0 +1,28 @@
+let ussd = 6;
+
+switch (ussd) {
+  case 1:
+    console.log("Data Plan");
+    break;
+  case 2:
+    console.log("Social Bundles");
+    break;
+  case 3:
+    console.log("Data Balance");
+    break;
+  case 4:
+    console.log("XtraValue");
+    break;
+  case 5:
+    console.log("Roaming/Int'l");
+    break;
+  case 6:
+    console.log("Borrow Credit/Data");
+    break;
+  case 7:
+    console.log("NEXT");
+    break;
+  default:
+    console.log("invalid code entered");
+    break;
+}
